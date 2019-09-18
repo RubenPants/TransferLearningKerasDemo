f2b44a4d0a794f92cb0046ec29df3b2cb622dfb0
RubenPants <broekxruben@gmail.com>
2019-09-18 21:16:30 +0200

    Fixed bug in data visualization

--- data/visualize.py
+++ data/visualize.py
@@ -4,6 +4,7 @@ visualize.py
 Visualize processed images.
 """
 import argparse
+import matplotlib.pyplot as plt
 
 from myutils import *
 
@@ -15,7 +16,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--folder', type=str, default=folder)
     parser.add_argument('--index', type=int, default=index)
-    parser.add_argument('--save', type=bool, default=False)
+    parser.add_argument('--save', type=bool, default=True)
     
     # Parse arguments
     args = parser.parse_args()
@@ -26,4 +27,11 @@ if __name__ == '__main__':
     
     # Plot the file
     save_path = folder + 'images/image{}'.format(args.index) if args.save else None
-    create_image(img, save_path=save_path, title='Image {i} - Truth: {t}'.format(i=args.index, t=label))
+    fig, ax = plt.subplots(1, 1, figsize=(3, 3))
+    create_image(array=img,
+                 ax=ax,
+                 title='Image {i} - Truth: {t}'.format(i=args.index, t=label))
+    if save_path:
+        plt.savefig(save_path)
+    plt.show()
+    plt.close()
